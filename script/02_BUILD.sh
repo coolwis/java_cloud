4559d19c1030179c9d6847c460311d373069d2df
coolwis <wisdombrave@gmail.com>
2022-01-07 15:21:00 +0900

    test

--- script/02_BUILD.sh
+++ script/02_BUILD.sh
@@ -4,4 +4,4 @@
 export PATH=$MAVEN/apache-maven-3.6.3/bin:$PATH
 rm -rf $PROJECT_DIR
 git clone $SOURCE_URL $PROJECT_DIR
-mvn -DfinalName=$JAR_NAME install -f $APP_DIR
+mvn -Dmaven.test.skip=true -DfinalName=$JAR_NAME install -f $APP_DIR
